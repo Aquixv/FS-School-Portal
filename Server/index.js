e83c40df3e0dcef906dfdf7286074d2240a4f797
Aquiii <dareakintundealamu@gmail.com>
2026-03-19 21:33:08 +0100

    First Push

---
+++ Server/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config(); 
+require('./connection');
+
+const User = require('./models/User');
+app.use(cors({
+  origin: ["http://localhost:5173", ""],
+  credentials: true
+}));
+app.use(express.json());
+
+const port = process.env.PORT || 1500; 
+
+app.get('/health', (req, res) => {
+    res.status(200).send('Server is alive and kicking! 🚀');
+});
+
+app.get('/', (req, res) => {
+    res.send('QuizMaster API Live');
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
