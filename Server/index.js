366d5ab93aca3bc3c9c9f7e4fe20119ff9a8ad9e
Aquiii <dareakintundealamu@gmail.com>
2026-03-20 23:26:21 +0100

    Something, i guess

--- Server/index.js
+++ Server/index.js
@@ -3,8 +3,12 @@ const app = express();
 const cors = require('cors');
 require('dotenv').config(); 
 require('./connection');
-
+const path = require('path');
 const student = require('./models/Student');
+
+app.use(express.static(path.join(__dirname, 'views')));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 app.use(cors({
   origin: ["http://localhost:5173", ""],
   credentials: true
@@ -15,6 +19,10 @@ const port = process.env.PORT || 1500;
 
 app.get('/health', (req, res) => {
     res.status(200).send('Server is alive and kicking! 🚀');
+});
+
+app.get('/signup', (req, res) => {
+    res.render('signup'); 
 });
 
 app.get('/', (req, res) => {
